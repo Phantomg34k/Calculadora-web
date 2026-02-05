835b38da6778d4293c3ee2e503c3fbad77bf138c
Phantomg34k <abdielivanmartinezesparza@gmail.com>
2026-02-05 12:48:39 -0600

    Programa de resta final

---
+++ Numeros.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Numeros {
+private int n1;
+private int n2;
+public int getN1(){
+	return n1;}
+public void sentN1(int n1) {
+	this.n1=n1;
+	
+}
+public int getN2(){
+	return n2;}
+public void sentN2(int n2) {
+	this.n2=n2;
+	
+}
+}
